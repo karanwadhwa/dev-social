42d8e600ce6e0b45b40d26e41d9e50bac0eff01f
karanwadhwa <wadhwa.karan96@gmail.com>
2018-04-23 15:10:25 +0530

    mongo connection

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+require("dotenv").config();
 const app = express();
 
-const port = 2000;
+mongoose
+  .connect(process.env.mongoURI)
+  .then(() => console.log("Connected to DB"))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 2000;
 
 app.get("/", (req, res) => res.send("dev-social"));
 
