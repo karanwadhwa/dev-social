10e9ed30dcf6a343328bd40ed4e650872d3c8eb4
karanwadhwa <wadhwa.karan96@gmail.com>
2018-04-27 19:19:57 +0530

    barebones login route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -13,13 +13,13 @@ router.get("/test", (req, res) => {
   res.json({ msg: "/api/auth/test route" });
 });
 
-// @route   POST /api/auth/test
+// @route   POST /api/auth/register
 // @desc    user registration route
 // @access  Public
 router.post("/register", (req, res) => {
   // search if a user with entered email already exists
   User.findOne({ email: req.body.email }).then(user => {
-    if (user) return res.status(400).json({ msg: "email already exists" });
+    if (user) return res.status(400).json({ error: "email already exists" });
     else {
       // generate new User object
       const newUser = new User({
@@ -41,6 +41,27 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route   POST /api/auth/login
+// @desc    user registration route
+// @access  Public
+router.post("/login", (req, res) => {
+  // Find user by email
+  User.findOne({ email: req.body.email }).then(user => {
+    if (!user) {
+      return res.status(404).json({ error: "User Not Found" });
+    }
+
+    // User found, validate password
+    bcrypt.compare(req.body.password, user.password).then(isMatch => {
+      if (isMatch) {
+        return res.status(200).json({ msg: "User Login Successful" });
+      } else {
+        return res.status(401).json({ error: "Password Incorrect" });
+      }
+    });
   });
 });
 
