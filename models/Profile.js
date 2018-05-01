83cfe353e03fa1d16a6c376242df33e0e82e6278
karanwadhwa <wadhwa.karan96@gmail.com>
2018-05-01 17:10:54 +0530

    FIX: Profile model

--- models/Profile.js
+++ models/Profile.js
@@ -1,7 +1,7 @@
-const mongoose = require(mongoose);
-const Schema = mongoose.Schema();
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
+const ProfileSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: "users"
@@ -111,4 +111,4 @@ const PostSchema = new Schema({
   }
 });
 
-module.exports = Profile = mongoose.model("profile", profileSchema);
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
