8c42f4efb429eef3ec34ca5fd3efcf804f5bf13e
karanwadhwa <wadhwa.karan96@gmail.com>
2018-05-27 16:12:23 +0530

    basic redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -9,12 +11,14 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
