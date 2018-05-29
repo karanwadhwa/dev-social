3637acb325e6a7e5f0ed1289921ac1a47a83d558
karanwadhwa <wadhwa.karan96@gmail.com>
2018-05-30 00:49:28 +0530

    FIX: redux devtools extension config in store setup

--- client/src/store.js
+++ client/src/store.js
@@ -5,13 +5,33 @@ import rootReducer from "./reducers";
 const initialState = {};
 const middleware = [thunk];
 
-const store = createStore(
+// basic redux store without devtools enabled
+/* const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+); */
+
+// basic setup for redux dev tools
+// breaks app when browser doesnt have redux dev tools installed
+/* const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
+); */
+
+// fix for the above issue
+// Advanced store setup
+// https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
