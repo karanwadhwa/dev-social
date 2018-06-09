4ac0c7fe0d8c8b6cc1da4bbe9bceaf02d2a8898c
karanwadhwa <wadhwa.karan96@gmail.com>
2018-06-09 19:59:41 +0530

    Basic Home Component

--- client/src/components/layout/Home.js
+++ client/src/components/layout/Home.js
@@ -3,10 +3,10 @@ import React from "react";
 export default () => {
   return (
     /* <!-- Content Wrapper. Contains page content --> */
-    <div class="content-wrapper" /* style="min-height: 926px;" */>
-      <div class="content-header">
-        <div class="container-fluid">
-          <h1 class="m-0 text-dark">Home</h1>
+    <div className="content-wrapper" /* style="min-height: 926px;" */>
+      <div className="content-header">
+        <div className="container-fluid">
+          <h1 className="m-0 text-dark">Home</h1>
           <div className="content">Main Content</div>
         </div>
       </div>
